703fa8c7c7b9a82603548b333daedf2bb8545f41
limenVTech <limen@vt.edu>
2019-09-23 15:19:47 -0400

    Initial commit for aspace_converter

---
+++ aspace_converter/aspace_converter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Script to convert DIPS metadata spreadsheets into ArchivesSpace input.
+Input is a DIPS formatted CSV spreadsheet. Output is a CSV that uses the
+proper header row and fields for upload to ArchivesSpace.
+"""
+
+# TODO: Don't forget to fix Excell spaces with "strip"
+
+import csv
+import sys
+from os import path
+
+def main():
+	print('Hello World!')
+
+if __name__ == __main__:
+	main()
+
+
---
+++ dips_collator/dips_collator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Script to collate poorly organized DIPS files into well-formed, individual DIPS objects.
+"""
+import os
+
+print("this does not do anything yet")
+
+
+
+
+
+
+
+
+
+
+
