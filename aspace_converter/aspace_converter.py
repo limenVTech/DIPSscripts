7800139f8d0d9107f57a4a64a489a0104c700850
limenVTech <limen@vt.edu>
2019-09-23 16:45:30 -0400

    Open with PyCharm

--- aspace_converter/aspace_converter.py
+++ aspace_converter/aspace_converter.py
@@ -4,16 +4,18 @@ Input is a DIPS formatted CSV spreadsheet. Output is a CSV that uses the
 proper header row and fields for upload to ArchivesSpace.
 """
 
+from os import path
+from time import strftime
+
 # TODO: Don't forget to fix Excell spaces with "strip"
 
-import csv
-import sys
-from os import path
+def get_file():
+    return "/Users/limen/Desktop/"
 
 def main():
-	print('Hello World!')
-
-if __name__ == __main__:
-	main()
+    dips_csv = get_file()
+    aspace_csv = path.join(path.dirname(dips_csv), f'as_import_{strftime("%Y%m%d_%H%M%S")}.csv')
 
 
+if __name__ == '__main__':
+    main()
